cf22b8eb4eb7ccba4f5739b2a41dd1e4407ccea3
Naimur Rahman <naimur2231@gmail.com>
2023-05-21 23:28:12 +0600

    hide the Firebase config keys

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDXrPc37iaXUlZAn0swfTwxfawvgWtjKSc",
-  authDomain: "toy-trackers.firebaseapp.com",
-  projectId: "toy-trackers",
-  storageBucket: "toy-trackers.appspot.com",
-  messagingSenderId: "796921871434",
-  appId: "1:796921871434:web:0ffa40638911f463ad7ab3"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
